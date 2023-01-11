7c52766fe00c9374915c014cab347f929b39033b
Stanislav Trendafilov <114089296+Stanislav-Trendafilov@users.noreply.github.com>
2023-01-11 10:43:00 +0200

    Stacks

---
+++ Stacks and Queues - Lab/5. Print Even Numbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5._Print_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[]arr=Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            Queue<int> queue = new Queue<int>(arr);
+            int br = 0;
+            while(queue.Count > 0) 
+            {
+                if(queue.Peek()%2==0)
+                {
+                    if (br != 0)
+                    { Console.Write($", {queue.Dequeue()}"); }
+                    else
+                    {
+                        Console.Write($"{queue.Dequeue()}");
+                        br++;
+                    }
+                }
+                else
+                {
+                    queue.Dequeue() ;
+                }
+
+            }
+        }
+    }
+}
---
+++ Stacks and Queues - Lab/8. Traffic Jam/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8._Traffic_Jam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Queue<string> queue = new Queue<string>();
+            string command =Console.ReadLine();
+            int count = 0;
+            while(command != "end")
+            {
+                if(command=="green")
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (queue.Count == 0)
+                        {
+                            break;
+                        }
+                        Console.WriteLine($"{queue.Dequeue()} passed!");
+                        count++;
+                       
+                    }
+                }
+                else 
+                {
+                    queue.Enqueue(command);
+                }
+                command= Console.ReadLine();
+            }
+            Console.WriteLine($"{count} cars passed the crossroads.");
+        }
+    }
+}
